46fbbc4c5a81aef1c569b037f4813bf9c68bc811
Tarek Adam <tarek.adam@fms-ea.com>
2024-10-14 10:16:19 +0200

    exo thread 1.1 corrige

--- ExosM07AdvancedJava/src/TryCatchExo/FoundException.java
+++ ExosM07AdvancedJava/src/TryCatchExo/FoundException.java
@@ -5,18 +5,13 @@
 
 package TryCatchExo;
 
+import java.sql.Date;
+
 public class FoundException {
 	
 	public static void main(String[] args) {
-		
-		try {
 		Date date = null;  // attribute date created and null "reference" is affected to it "empty reference"
-		Date today = new Date(); //new date "object" is instantiated and assigned to "variable" today class Date
-		}catch(Exception e) { System.out.println("Something went wrong");}
-
-		try {
-		System.out.println(date.getClass().getName());
-		}catch(Exception e) { System.out.println("Something went wrong");}
+		Date today = new Date(0); //new date "object" is instantiated and assigned to "variable" today class Date
 		
 		try {
 		System.out.println(date.getClass().getName());
@@ -26,27 +21,5 @@ public class FoundException {
 	
 
 }
-//
-//Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
-//	Date cannot be resolved to a type
-//	Date cannot be resolved to a type
-//	Date cannot be resolved to a type
-//
-//	at TryCatckExo.FoundException.main(FoundException.java:13)
-
-//*************************************************************
-
-//Using an uninitialized date variable:
-//
-//    The date variable is initialized to null, which means that it does not refer to any object.
-//    When calling date.getClass().getName() a NullPointerException will be thrown 
-//	  because date is null and you are trying to access a method on a null reference.
-//
-//Potential error importing the Date class://
-//    You need to import the Date class from the java.util.Date package (or java.sql.Date if that's what you want to use). Otherwise, a compilation error will occur because Java will not know which Date class it is.
-//
-//getClass() method://
-//    The getClass() method is a method inherited from the Object base class in Java. Any class 
-//    in Java implicitly inherits from the Object class, which allows any instance to access the getClass() method.
 
 
